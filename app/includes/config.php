8ac8335de5ea115a7e14a1cebad8e67b33938321
Alessandro <alessandrodezuani@outlook.it>
2025-05-09 19:14:10 +0200

    Rimuovi le chiavi sensibili da config.php e svuota i campi delle credenziali SMTP

--- app/includes/config.php
+++ app/includes/config.php
@@ -7,20 +7,20 @@ define('DB_NAME', 'loginsystem');
 define('DB_PORT', 3306); // Aggiungi la porta se non è la predefinita 3306
 
 // --- Chiavi Google reCAPTCHA v2 Checkbox ---
-define('RECAPTCHA_SITE_KEY', '6Ld5eyErAAAAAN3fWiUC1f53DDe9pAnZRXvZ8bF-'); // Sostituisci con la tua Site Key
-define('RECAPTCHA_SECRET_KEY', '6Ld5eyErAAAAAJXol4GEEqDvbzoBnTArf8xjx-CS'); // Sostituisci con la tua Secret Key
+define('RECAPTCHA_SITE_KEY', '-'); // Sostituisci con la tua Site Key
+define('RECAPTCHA_SECRET_KEY', '-'); // Sostituisci con la tua Secret Key
 
 // --- Impostazioni PHPMailer (SMTP) ---
 define('SMTP_HOST', 'smtp.gmail.com');          // Host SMTP (es. 'smtp.gmail.com' o il tuo)
 define('SMTP_AUTH', true);                     // Abilita autenticazione SMTP (true/false)
-define('SMTP_USER', 'dezuani.fotovoltaico@gmail.com'); // Username SMTP (la tua email)
-define('SMTP_PASS', 'ymzf ceed cgvr tpga');          // Password SMTP (la tua password o App Password)
+define('SMTP_USER', ''); // Username SMTP (la tua email)
+define('SMTP_PASS', '');          // Password SMTP (la tua password o App Password)
 define('SMTP_SECURE', 'tls');                  // Tipo di crittografia (tls o ssl) - Usa PHPMailer::ENCRYPTION_STARTTLS o PHPMailer::ENCRYPTION_SMTPS
 define('SMTP_PORT', 587);                      // Porta TCP per la connessione (587 per TLS, 465 per SSL)
 define('MAIL_CHARSET', 'UTF-8');               // Set di caratteri per l'email
 
 // --- Impostazioni Mittente Email ---
-define('MAIL_FROM', 'dezuani.fotovoltaico@gmail.com'); // Indirizzo email mittente
+define('MAIL_FROM', ''); // Indirizzo email mittente
 define('MAIL_FROM_NAME', 'Asi-Castello');         // Nome mittente
 
 // --- URL Base Applicazione (per link nelle email) ---
